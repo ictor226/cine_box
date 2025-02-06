22288aec1e84ce13793056ab48fbff57c4b1fe25
Victor Huggo <huggoV226@gmail.com>
2025-02-06 11:57:42 -0300

    adicionaei php para ficar  mais fluido

--- CINE_BOX/includes/header.php
+++ CINE_BOX/includes/header.php
@@ -3,7 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CineBox</title>
+    <title>
+    <?php
+    if (isset ($titulo) &&  !empty($titulo)){
+        echo $titulo;
+
+    } else{
+
+        echo 'CineBox';
+    }
+    ?>
+    
+    </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/style.css">
@@ -14,12 +25,12 @@
     <!-- inicio -->
     <header>
         <nav>
-            <a href="#" class="logo">
+            <a href="./index.php" class="logo">
                 <h1>CineBox</h1>
             </a>
             <ul class="menu">
-                <li><a href="#">Inicio</a></li>
-                <li><a href="#">filmes</a></li>
+                <li><a href="./index.php">Inicio</a></li>
+                <li><a href="./listarfilmes.php">filmes</a></li>
                 <li><a href="#">sobre</a></li>
 
             </ul>
--- CINE_BOX/index.php
+++ CINE_BOX/index.php
@@ -1,4 +1,7 @@
-<?php include'./includes/Header.php' ?>
+<?php
+$titulo = 'CineBox - Inicio';
+include './includes/Header.php' ?>
+
 <section id="baner">
     <main class="carrossel-container">
         <div class="carrossel">
@@ -12,13 +15,17 @@
         </div>
         <BUtton class="anterior" onclick="javascript:anteriorSlide()"> <i class="bi bi-arrow-left-short"></i> </BUtton>
         <BUtton class="proximo" onclick="javascript:proximoSlide()"><i class="bi bi-arrow-right-short"></i></BUtton>
-
     </main>
 </section>
 <section id="filmes-recomendados">
     <h2 class="titulo">filmes</h2>
     <main class="container ">
         <div class="row">
+
+        <?php
+        for ($i=0; $i <8; $i++) { 
+            
+        ?>
 
             <div class=" col col-3">
                 <figure>
@@ -38,143 +45,7 @@
                     </span>
                 </figure>
             </div>
-
-
-            <div class=" col col-3">
-                <figure>
-                    <img src="./assets/img/poster/gente-grande-2.png" alt="poster do filme gente grande 2"
-                        class="foto-produto">
-                    <figcaption>
-                        <h4>gente grande 2</h4>
-                        <span class="preco"> R$ 120,00</span>
-                        <P class="descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas et
-                            veritatis numquam, voluptatem esse similique ea, eaque maxime impedit alias fugit
-                            repudiandae aliquid quod a optio voluptas explicabo laborum fuga?</P>
-                    </figcaption>
-                    <span class="genero">
-                        <label style="background-color: #070373;">Fantasia</label>
-                        <label style="background-color: #580259;">Drama</label>
-                        <label style="background-color: #d966ba;">Ficção</label>
-                    </span>
-                </figure>
-            </div>
-
-
-            <div class=" col col-3">
-                <figure>
-                    <img src="./assets/img/poster/bad-boys-ate-o-fim.png" alt="poster do filme bad boys ate o fim"
-                        class="foto-produto">
-                    <figcaption>
-                        <h4>Bad Boys Até o Fim</h4>
-                        <span class="preco"> R$ 139,00</span>
-                        <P class="descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas et
-                            veritatis numquam, voluptatem esse similique ea, eaque maxime impedit alias fugit
-                            repudiandae aliquid quod a optio voluptas explicabo laborum fuga?</P>
-                    </figcaption>
-                    <span class="genero">
-                        <label style="background-color: #070373;">Fantasia</label>
-                        <label style="background-color: #580259;">Drama</label>
-                        <label style="background-color: #d966ba;">musical</label>
-                    </span>
-                </figure>
-            </div>
-
-
-            <div class=" col col-3">
-                <figure>
-                    <img src="./assets/img/poster/coringa-2.png" alt="poster do filme coringa 2" class="foto-produto">
-                    <figcaption>
-                        <h4>Coringa 2</h4>
-                        <span class="preco"> R$ 139,90</span>
-                        <P class="descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas et
-                            veritatis numquam, voluptatem esse similique ea, eaque maxime impedit alias fugit
-                            repudiandae aliquid quod a optio voluptas explicabo laborum fuga?</P>
-                    </figcaption>
-                    <span class="genero">
-                        <label style="background-color: #070373;">Fantasia</label>
-                        <label style="background-color: #580259;">Drama</label>
-                        <label style="background-color: #d966ba;">musical</label>
-                    </span>
-                </figure>
-            </div>
-
-
-            <div class=" col col-3">
-                <figure>
-                    <img src="./assets/img/poster/pulp-fiction.png" alt="poster do filme oulp fiction"
-                        class="foto-produto">
-                    <figcaption>
-                        <h4>Pulp Fiction</h4>
-                        <span class="preco"> R$ 150,00</span>
-                        <P class="descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas et
-                            veritatis numquam, voluptatem esse similique ea, eaque maxime impedit alias fugit
-                            repudiandae aliquid quod a optio voluptas explicabo laborum fuga?</P>
-                    </figcaption>
-                    <span class="genero">
-                        <label style="background-color: #070373;">Fantasia</label>
-                        <label style="background-color: #580259;">Drama</label>
-                        <label style="background-color: #d966ba;">Ficção</label>
-                    </span>
-                </figure>
-            </div>
-
-
-            <div class=" col col-3">
-                <figure>
-                    <img src="./assets/img/poster/sem-tempo-para-morrer.png" alt="poster do filme Sem tempo para morrer"
-                        class="foto-produto">
-                    <figcaption>
-                        <h4>Sem tempo para morrer</h4>
-                        <span class="preco"> R$ 500,00</span>
-                        <P class="descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas et
-                            veritatis numquam, voluptatem esse similique ea, eaque maxime impedit alias fugit
-                            repudiandae aliquid quod a optio voluptas explicabo laborum fuga?</P>
-                    </figcaption>
-                    <span class="genero">
-                        <label style="background-color: #070373;">Fantasia</label>
-                        <label style="background-color: #580259;">Drama</label>
-                        <label style="background-color: #d966ba;">Ficção</label>
-                    </span>
-                </figure>
-            </div>
-
-
-            <div class=" col col-3">
-                <figure>
-                    <img src="./assets/img/poster/star-wars-the-last-jedi.png"
-                        alt="poster do filme  star wars the last jedi" class="foto-produto">
-                    <figcaption>
-                        <h4>Star Wars The Last Jedi</h4>
-                        <span class="preco"> R$ 90,00</span>
-                        <P class="descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas et
-                            veritatis numquam, voluptatem esse similique ea, eaque maxime impedit alias fugit
-                            repudiandae aliquid quod a optio voluptas explicabo laborum fuga?</P>
-                    </figcaption>
-                    <span class="genero">
-                        <label style="background-color: #070373;">Fantasia</label>
-                        <label style="background-color: #d966ba;">Ficção</label>
-                    </span>
-                </figure>
-            </div>
-
-
-            <div class=" col col-3">
-                <figure>
-                    <img src="./assets/img/poster/the-batman.png" alt="poster do filme The Batman" class="foto-produto">
-                    <figcaption>
-                        <h4>The Batman</h4>
-                        <span class="preco"> R$ 290,00</span>
-                        <P class="descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas et
-                            veritatis numquam, voluptatem esse similique ea, eaque maxime impedit alias fugit
-                            repudiandae aliquid quod a optio voluptas explicabo laborum fuga?</P>
-                    </figcaption>
-                    <span class="genero">
-                        <label style="background-color: #070373;">Fantasia</label>
-                        <label style="background-color: #580259;">Drama</label>
-                    </span>
-                </figure>
-            </div>
-
+            <?php } ?>
         </div>
     </main>
 </section>
