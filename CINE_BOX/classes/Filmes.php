adcd42e548db4ec93f12a599efbf4df648257f94
Victor Huggo <huggoV226@gmail.com>
2025-03-28 11:25:24 -0300

    test

--- CINE_BOX/classes/Filmes.php
+++ CINE_BOX/classes/Filmes.php
@@ -3,46 +3,40 @@ class Filmes
 {
     public $conexaoBanco;
 
-    public function __construct() {
-        
+    public function __construct()
+    {
+
         $dsn = 'mysql:dbname=db_cinebox;host=127.0.0.1';
-        $user = 'root'; 
+        $user = 'root';
         $password = '';
         $auxScript = '';
-        
+
 
         $this->conexaoBanco = new PDO($dsn, $user, $password);
-        
-
     }
-    
-        public function exibirlistaFilmes($limite = ''){
-            
 
-$auxScript = '';
+    public function exibirlistaFilmes($limite = '')
+    {
 
-            if(!empty($limite))
-            {
 
-                $auxScript = " ORDER BY RAND() LIMIT {$limite}";
+        $auxScript = '';
 
-            }
+        if (!empty($limite)) {
 
-            
-         
-
-            $script = 'SELECT * FROM tb_filmes' .$auxScript;
-
-            return  $this->conexaoBanco->query($script)->fetchAll();
-            
-            
-
+            $auxScript = " ORDER BY RAND() LIMIT {$limite}";
         }
 
-        public function exibirDetalhesFilmes()
+
+
+
+        $script = 'SELECT * FROM tb_filmes' . $auxScript;
+
+        return  $this->conexaoBanco->query($script)->fetchAll();
+    }
+
+    public function exibirDetalhesFilmes()
     {
         $id = $_GET['id'];
- 
         $script = "SELECT tb_generos.cor, tb_filmes.*, tb_filme_genero.*
         FROM tb_filme_genero
         INNER JOIN tb_filmes
@@ -50,9 +44,7 @@ $auxScript = '';
         INNER JOIN tb_generos
         ON tb_generos.id=tb_filme_genero.genero_id
         WHERE tb_filme_genero.filme_id = {$id}";
- 
+
         return $this->conexaoBanco->query($script)->fetch();
     }
-
-
 }
